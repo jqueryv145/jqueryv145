6f5ade024a222f0f54dce961c338e8f00daddf5b
huukhai <huukhai@sepay.vn>
2025-12-03 07:54:34 +0000

    save

--- data.js
+++ data.js
@@ -1,24 +1,13 @@
 const BACKLINK_DATA = {
     AUTO_BACKLINKS: [
         'https://worldbook.io.vn',
-        'https://worldbook.io.vn/about',
     ],
     HIDDEN_BACKLINKS: [
         'https://fnbowner.com.vn',
         'https://khaizinam.io.vn',
         'https://manga18k.xyz',
-        'https://manga18k.xyz/about',
-        'https://manga18k.xyz/contact',
-        'https://manga18k.xyz/privacy',
-        'https://manga18k.xyz/terms',
-        'https://manga18k.xyz/dmca',
         'https://teamsohoa.com',
         'https://worldbook.io.vn',
-        'https://worldbook.io.vn/about',
-        'https://worldbook.io.vn/contact',
-        'https://worldbook.io.vn/privacy',
-        'https://worldbook.io.vn/terms',
-        'https://worldbook.io.vn/dmca',
     ]
 };
 
