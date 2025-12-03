88c0c77f8d555ab5c4c56743a62fa398e3b39e1f
huukhai <huukhai@sepay.vn>
2025-12-03 07:03:03 +0000

    save

--- data.js
+++ data.js
@@ -1,19 +1,24 @@
 const BACKLINK_DATA = {
     AUTO_BACKLINKS: [
         'https://worldbook.io.vn',
-        'https://worldbook.io.vn/novel/9731',
-        'https://worldbook.io.vn/novel/9629',
-        'https://fnbowner.com.vn'
+        'https://worldbook.io.vn/about',
     ],
     HIDDEN_BACKLINKS: [
         'https://fnbowner.com.vn',
         'https://khaizinam.io.vn',
         'https://manga18k.xyz',
-        'https://manga18k.xyz/story/a-boss-who-is-so-kind-to-me-raw',
-        'https://manga18k.xyz/story/the-gift-of-evil',
+        'https://manga18k.xyz/about',
+        'https://manga18k.xyz/contact',
+        'https://manga18k.xyz/privacy',
+        'https://manga18k.xyz/terms',
+        'https://manga18k.xyz/dmca',
+        'https://teamsohoa.com',
         'https://worldbook.io.vn',
-        'https://worldbook.io.vn/novel/9731',
-        'https://worldbook.io.vn/novel/9629'
+        'https://worldbook.io.vn/about',
+        'https://worldbook.io.vn/contact',
+        'https://worldbook.io.vn/privacy',
+        'https://worldbook.io.vn/terms',
+        'https://worldbook.io.vn/dmca',
     ]
 };
 
